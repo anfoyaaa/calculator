28d84896510c291638d55777fa55f60726175942
anfoyaaa <124536077+anfoyaaa@users.noreply.github.com>
2023-02-23 12:46:37 +0300

    Добавьте файлы проекта.

---
+++ calc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace calculator;
+
+class calc
+{
+    static void Main(string[] args)
+    {
+        double firstValue, secondValue;
+        string action;
+
+        Console.WriteLine("Введите число 1");
+        firstValue = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите число 2");
+        secondValue = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Выберите операции: '*', '/', '-', '+', '**' ");
+        action= Console.ReadLine();
+
+        switch (action)
+        {
+            case "+":
+                Console.WriteLine(firstValue + secondValue);
+                break;
+
+            case "-":
+                Console.WriteLine(firstValue - secondValue);
+                break;
+
+            case "*":
+                Console.WriteLine(firstValue * secondValue);
+                break;
+
+            case "**":
+                Console.WriteLine(Math.Pow(firstValue, secondValue));
+                break;
+
+            case "/":
+                if(secondValue == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(firstValue / secondValue);
+                }
+                
+                break;
+            default:
+                Console.WriteLine("Вы ввели не правильно");
+                break;
+
+
+
+        }
+    }
+
+}
+
